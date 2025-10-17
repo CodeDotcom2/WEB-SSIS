4c1048f1b4ec908456cd3c00510f2ab429e92737
Hiyoshi <197499394+CodeDotcom2@users.noreply.github.com>
2025-10-17 23:18:14 +0800

    feat: enhance backend initialization (build) and serve frontend files

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 from . import database
 import setup_db
+import os
 from .config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, SECRET_KEY
 
 login_manager = LoginManager()
@@ -23,15 +24,13 @@ def create_app():
     login_manager.login_view = 'user.login'
 
     from .user import user_bp
-    app.register_blueprint(user_bp)
-
     from .student import student_bp
-    app.register_blueprint(student_bp)
-
     from .college import college_bp
-    app.register_blueprint(college_bp)
-
     from .program import program_bp
+
+    app.register_blueprint(user_bp)
+    app.register_blueprint(student_bp)
+    app.register_blueprint(college_bp)
     app.register_blueprint(program_bp)
 
     setup_db.create_tables()
@@ -40,5 +39,17 @@ def create_app():
     def load_user(user_id):
         from .models import Users
         return Users.get_by_id(user_id)
+    
+
+    frontend_dir = os.path.join(os.path.dirname(__file__), "../../frontend/out")
+
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve_frontend(path):
+        file_path = os.path.join(frontend_dir, path)
+        if os.path.isfile(file_path):
+            return send_from_directory(frontend_dir, path)
+        else:
+            return send_from_directory(frontend_dir, "index.html")
 
     return app
